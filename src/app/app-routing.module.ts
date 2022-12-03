de4e34b886887a9af76aa4fe482c8cd2896cdd0a
Tamara Dzambic <dzambic.sv43.2020@uns.ac.rs>
2022-12-04 00:37:44 +0100

    Login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { MapComponent } from './components/map/map.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'login', component: LoginComponent},
+  {path : 'register', component: RegistrationComponent},
+  {path : 'home', component: MapComponent},
+  {path : '**', component: MapComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
