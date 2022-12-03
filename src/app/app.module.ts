2305defcc717eae60e5511c6f0c618c03471340f
afilipic <75852858+afilipic@users.noreply.github.com>
2022-12-03 18:47:01 +0100

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { MapComponent } from './map/map.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    MapComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
