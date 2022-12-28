2ee0ac65ef768dbdd845acdabaca1193171811af
Anita Pajic <anitaapajic@gmail.com>
2022-12-28 20:06:52 +0100

    Added connection with backend for login
    Added connection with backend for login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from 'node_modules/@angular/router';
 
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/auth/login/login.component';
 import { MapComponent } from './components/map/map/map.component';
 import { MapModule } from './components/map/map/map.module';
-import { RegistrationComponent } from './components/registration/registration.component';
+import { RegistrationComponent } from './components/auth/registration/registration.component';
 
 const routes: Routes = [
   {path : 'login', component: LoginComponent},
