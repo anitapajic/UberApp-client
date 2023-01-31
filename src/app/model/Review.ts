e683b7938127bc28612ae615c6e6365219041e4e
afilipic <75852858+afilipic@users.noreply.github.com>
2023-01-31 01:47:52 +0100

    Add ratings for ride

--- src/app/model/Review.ts
+++ src/app/model/Review.ts
@@ -1,1 +1,12 @@
-export interface Review{}
+export interface Review{
+    rating: number;
+    rideId: number;
+    comment: string;
+    driver: number;
+}
+export interface ReviewDTO{
+    rating: number;
+    ride: number;
+    comment: string;
+    driver: number;
+}
